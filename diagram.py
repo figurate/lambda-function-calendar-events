59746e2830420263b49db17aed993aed513f3845
Ben Fortuna <bfortuna@odecee.com.au>
2021-02-11 19:21:10 +1100

    Updated design docs

--- diagram.py
+++ diagram.py
@@ -1,8 +1,13 @@
 from diagrams import Diagram
 from diagrams.aws.compute import Lambda
 from diagrams.aws.database import DynamodbTable
+from diagrams.aws.integration import SNS
 from diagrams.aws.network import APIGateway
 
 with Diagram("Calendar Events", show=False, direction="TB"):
 
-    APIGateway("calendar events api") >> Lambda("calendar events") >> DynamodbTable("calendar events")
+    events = Lambda("calendar events")
+    topic = SNS("calendar topic")
+
+    APIGateway("calendar events api") >> events >> DynamodbTable("calendar events") >> events
+    events >> topic
